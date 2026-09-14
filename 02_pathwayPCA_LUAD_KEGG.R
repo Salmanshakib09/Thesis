0491c26b1b63c95c1dd5457663db9158e53b7a1b
Salman Shakib <salmanshakib@Salmans-MacBook-Air.local>
2026-09-14 18:07:01 -0400

    code change

--- 02_pathwayPCA_LUAD_KEGG.R
+++ 02_pathwayPCA_LUAD_KEGG.R
@@ -1,5 +1,5 @@
 library(pathwayPCA)
-
+library(tidyverse)
 # Read processed TCGA-LUAD data
 luad_data <- readRDS(
   "data/processed/tcga_luad_processed.rds"
@@ -7,19 +7,14 @@ luad_data <- readRDS(
 
 # Read KEGG pathways
 kegg_pathways <- read_gmt(
-  "data/raw/pathways/c2.cp.kegg_medicus.v2026.1.Hs.symbols.gmt",
-  description = TRUE
-)
-luad_assay <- luad_data[, -c(2, 3)]
-luad_survival <- luad_data[, c(
-  "patient_id",
-  "OS_time",
-  "OS_event"
-)]
-dim(luad_assay)
-dim(luad_survival)
+  "data/pathways/kegg_medicus.gmt",
+  description=TRUE
+  )
+luad_assay <- luad_data|>
+select(-OS_time,-OS_event)
+luad_survival= luad_data|>
+  select(patient_id,OS_time,OS_event)
 
-head(luad_survival)
 luad_omics <- CreateOmics(
   assayData_df = luad_assay,
   pathwayCollection_ls = kegg_pathways,
@@ -37,11 +32,9 @@ luad_aespc <- AESPCA_pVals(
   adjustpValues = TRUE,
   adjustment = "BH"
 )
-getPathpVals(luad_aespc, numPaths = 20)
-top_paths <- getPathpVals(
-  luad_aespc,
-  numPaths = 20
-)
+top_paths= getPathpVals(luad_aespc, 
+                        numPaths = 20
+                        )
 top_path <- top_paths$terms[1]
 
 top_path
