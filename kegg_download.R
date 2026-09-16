7700576a9a0fd276925c3fb29b763b59c468a1d4
Salman Shakib <salmanshakib@Salmans-MacBook-Air.local>
2026-09-16 04:55:39 -0400

    data download

--- kegg_download.R
+++ kegg_download.R
@@ -1,10 +1,22 @@
+library(tidyverse)
 library(msigdbr)
-library(readr)
-kegg= msigdbr(db_species = "HS", species = "Homo sapiens", 
-              collection ="C2", subcollection ="CP:KEGG_MEDICUS")
-unique(kegg$db_version)
+kegg= msigdbr(db_species = "HS", 
+              species = "Homo sapiens", 
+              collection = "C2",
+              subcollection = "CP:KEGG_MEDICUS"
+              )
+kegg_gmt= kegg |> 
+          select(gs_name, gs_description, gene_symbol)|>
+          distinct()|>
+          group_by(gs_name, gs_description)|>
+          summarise(genes= paste(gene_symbol, collapse = "\t"),
+                    .groups = "drop"
+          )|>
+          mutate(
+            line=paste(gs_name, gs_description, genes, sep = "/t")
+          )
 dir.create("data/pathways",
-           recursive = TRUE, 
+           recursive=TRUE,
            showWarnings = FALSE)
-write_csv(kegg,
-          "data/pathways/kegg_medicus.csv")
+write_lines( kegg_gmt$line,
+            "data/pathways/kegg_medicus.gmt")
